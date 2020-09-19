fb3e40934683d86e9936ae894d5d18c068cd4e0e
Marta Fajlhauer <fajlhauermarta@gmail.com>
2020-09-19 10:36:29 +0100

    Add new file and start first changes

--- hello.py
+++ hello.py
@@ -1,9 +1,2 @@
 print("Hello")
 
-import pandas as pd
-
-data = pd.read_csv("C:/Users/fajlh/OneDrive/Documents/IBM_DS/Data-Collisions.csv", low_memory=False)
-
-print(data.head())
-
-print(data.columns)
