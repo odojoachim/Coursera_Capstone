46eaae15d56a7302e985896d9f827e688782e187
Marta Fajlhauer <fajlhauermarta@gmail.com>
2020-09-19 10:30:02 +0100

    updating test file

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
 print("Hello")
+
+import pandas as pd
+
+data = pd.read_csv("C:/Users/fajlh/OneDrive/Documents/IBM_DS/Data-Collisions.csv", low_memory=False)
+
+print(data.head())
+
+print(data.columns)
